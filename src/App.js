3bb7a1445525966905ee9d7e29300300bd2ee186
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-15 14:21:25 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Expenses from "./components/Expenses/Expenses";
+import "./App.css";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Internet Bill",
+      amount: 500,
+      date: new Date(2022, 9, 10),
+    },
+    {
+      id: "e2",
+      title: "Electricity Bill",
+      amount: 1459,
+      date: new Date(2022, 9, 5),
+    },
+    {
+      id: "e3",
+      title: "Netflix Bill",
+      amount: 750,
+      date: new Date(2022, 9, 14),
+    },
+    {
+      id: "e4",
+      title: "Shopping",
+      amount: 3500,
+      date: new Date(2022, 9, 15),
+    },
+  ];
+
+  return (
+    <div className="App">
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
